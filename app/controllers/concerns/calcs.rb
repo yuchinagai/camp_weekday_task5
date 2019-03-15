0082951cf318a15c89bb1a2b29825eb3ab9f8a88
YuchiNagai <alone.on.the.rope1231@gmail.com>
2019-03-15 15:57:17 +0900

    軽微な修正

--- app/controllers/concerns/calcs.rb
+++ app/controllers/concerns/calcs.rb
@@ -12,46 +12,44 @@ module Calcs
 
     # 星座判別
     def zodiac
-      case @monthday
-      when (101..119) then
-        @zodiac = "山羊座"
-      when (120..218) then
-        @zodiac = "水瓶座"
-      when (219..320) then
-        @zodiac = "魚座"
-      when (321..419) then
-        @zodiac = "牡羊座"
-      when (420..520) then
-        @zodiac = "牡牛座"
-      when (521..621) then
-        @zodiac = "双子座"
-      when (622..722) then
-        @zodiac = "蟹座"
-      when (723..822) then
-        @zodiac = "獅子座"
-      when (823..922) then
-        @zodiac = "獅子座"
-      when (923..1023) then
-        @zodiac = "天秤座"
-      when (1024..1122) then
-        @zodiac = "蠍座"
-      when (1123..1221) then
-        @zodiac = "射手座"
-      when (1222..1231) then
-        @zodiac = "山羊座"
-      else
-      end
+      @zodiac = case @monthday
+                when (101..119) 
+                  "山羊座"
+                when (120..218) 
+                  "水瓶座"
+                when (219..320) 
+                  "魚座"
+                when (321..419) 
+                  "牡羊座"
+                when (420..520) 
+                  "牡牛座"
+                when (521..621) 
+                  "双子座"
+                when (622..722) 
+                  "蟹座"
+                when (723..822) 
+                  "獅子座"
+                when (823..922) 
+                  "乙女座"
+                when (923..1023) 
+                  "天秤座"
+                when (1024..1122) 
+                  "蠍座"
+                when (1123..1221) 
+                  "射手座"
+                when (1222..1231) 
+                  "山羊座"
+                end
     end
 
     # 年齢判別
     def age
-    @age = (Date.today.strftime("%Y%m%d").to_i - @DOB.to_i) / 10000  
+      @age = (Date.today.strftime("%Y%m%d").to_i - @DOB.to_i) / 10000  
     end
 
     # 運勢
     def fortune
-      fortune = ["大吉", "中吉", "吉", "小吉", "凶", "大凶"]
-      @fortune = fortune.sample
+      @fortune = %w(大吉 中吉 吉 小吉 凶 大凶).sample
     end
   end
 end
